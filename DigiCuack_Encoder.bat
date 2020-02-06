1f4d0194960595543bb159f47fa7feb61a0dad9f
Liam-McHara <guillemlr@pm.me>
2020-02-06 14:06:30 +0100

    v1.4>  New version!!
    - now in propar English
    - Now it handles spaces in filenames as expected (unless you try to do very weird things)

--- DigiCuack_Encoder.bat
+++ DigiCuack_Encoder.bat
@@ -1,7 +1,9 @@
 @echo off
-:: Versión del 2 de febrero del 2020
+setlocal enableextensions enabledelayedexpansion
 
-Title DigiCuack Encoder (1.3)
+:: Versión del 6 de febrero del 2020
+
+Title DigiCuack Encoder (1.4)
 
 :Start
 echo "______ _       _ _____                  _      _____                    _           "
@@ -11,18 +13,17 @@ echo "| | | | |/ _\ | | |   | | | |/ _\ |/ __| |/ / |  __| '_ \ / __/ _ \ / _\ |
 echo "| |/ /| | (_| | | \__/\ |_| | (_| | (__|   <  | |__| | | | (_| (_) | (_| |  __/ |   "
 echo "|___/ |_|\__, |_|\____/\__,_|\__,_|\___|_|\_\ \____/_| |_|\___\___/ \__,_|\___|_|   "
 echo "          __/ |                                                                     "
-echo "         |___/                                        v 1.3      By: Liam McHara    "
+echo "         |___/                                        v 1.4      By: Liam McHara    "
 echo.
 
 :: Salta la selección si se envía como parámetro
 if NOT [%1]==[] set payload=%1 && goto Selected
 
 ::Selección
-echo Se han encontrado los siguientes payloads:
+echo I've found these payloads:
 echo.
 
 :: Lista los archivos .duck de la carpeta Payloads (sin la extensión), los cuenta (cantidad)
-setlocal enableextensions enabledelayedexpansion
 set /a cantidad=0
 for %%a in ("Payloads\*.duck") do (
 	set /a cantidad += 1 
@@ -30,7 +31,7 @@ for %%a in ("Payloads\*.duck") do (
 )
 echo.
 
-set /p "numero=Escribe el numero del payload que quieras cargar: " || cls && goto Start
+set /p "numero=Select the payload you wish to load (write its number): " || cls && goto Start
 
 set /a i=1
 for %%a in ("Payloads\*.duck") do (
@@ -38,50 +39,48 @@ for %%a in ("Payloads\*.duck") do (
 	set /a i += 1 
 )
 
-endlocal && set payload=%payload%
 if not defined payload cls && goto Start
 
 :Selected
-:: Elimina espacios en la variable 
-set payload=%payload: =%
-echo Has seleccionado: %payload%
+:: Removes spaces at end of filename (needed if payload sent as a parameter)
+
+if "!payload:~-1!"==" " set payload=!payload:~0,-1!
+
+echo Selected payload: "%payload%"
 echo.
 
-:: CODIFICACION
 :: 0. borrado de datos anteriores (just in case, para que tenga sentido el paso 2)
 del raw.bin >nul 2>nul
 
 :: 1. codifiacion (sin echo)
-java -jar encoder.jar -i Payloads\%payload%.duck" -o raw.bin -l es >nul 2>nul
+java -jar encoder.jar -i "Payloads\%payload%.duck" -o raw.bin -l es >nul 2>nul
 
 :: 2. comprobación
 IF NOT EXIST raw.bin (goto Error)
 
 :: 3. digi-coding
-echo Codificacion inicial - OK!
-rmdir /Q /S Sketches\%payload% >nul 2>nul
-mkdir Sketches\%payload%\sketch
-duck2spark.py -i raw.bin -l 1 -f 2000 -o Sketches\%payload%\sketch\sketch.ino
-echo Conversion a Digispark - OK!
-echo|set /p="Iniciando Arduino IDE... "
+echo Initial coding - OK!
+rmdir /Q /S "Sketches\%payload%" >nul 2>nul
+mkdir "Sketches\%payload%\sketch"
+duck2spark.py -i raw.bin -l 1 -f 2000 -o "Sketches\%payload%\sketch\sketch.ino"
+echo Conversion to Digispark - OK!
+echo|set /p="Starting the Arduino thingy... "
 
 :: Método con GUI 
 :: Sketches\%payload%\sketch\sketch.ino
 
 :: Método sin GUI:
-"C:\Program Files (x86)\Arduino\arduino_debug.exe" --upload Sketches\%payload%\sketch\sketch.ino 
-::| find "Sketch" > out.txt
+"C:\Program Files (x86)\Arduino\arduino_debug.exe" --upload "Sketches\%payload%\sketch\sketch.ino" 
 
 echo OK!
 goto Salir
 
 
 :Error
-echo ERROR: Aprende a escribir!
-set /p "ans=Otra oportunidad? (S/n)" || cls && goto Start
+echo ERROR: Wrong input!
+set /p "ans=Try again? (Y/n)" || cls && goto Start
 if %ans%==n goto Salir
-cls
-goto Start
+cls && goto Start
 
 :Salir
 echo.
